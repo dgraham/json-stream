5955fa5c00bd3907e88f0638d51f9cdded6aa32a
David Graham <david.malcom.graham@gmail.com>
2014-07-05 20:53:06 -0600

    Exponents always result in Floats.
    Matches json gem, yajl-ruby, and Ruby literal behavior.

--- lib/json/stream/parser.rb
+++ lib/json/stream/parser.rb
@@ -44,7 +44,6 @@ module JSON
       PLUS          = '+'
       POINT         = '.'
       EXPONENT      = /[eE]/
-      FLOAT         = /[\.eE]/
       B,F,N,R,T,U   = %w[b f n r t u]
 
       # Parses a full JSON document from a String or an IO stream and returns
@@ -331,8 +330,7 @@ module JSON
             else
               error('Expected 0-9 digit') unless @buf =~ DIGIT_END
               @state = :end_value
-              number = (@buf =~ FLOAT) ? @buf.to_f : @buf.to_i
-              notify(:value, number)
+              notify(:value, @buf.to_f)
               @buf = ""
               @pos -= 1
               redo
